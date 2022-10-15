0a614fe07a2c58525dc36e8bdcdc8f8c94e0f25c
Evan Woods <22337004+efwoods@users.noreply.github.com>
2022-10-15 19:41:27 -0400

    commit

--- yt_download.py
+++ yt_download.py
@@ -1,7 +1,20 @@
 from pytube import YouTube
 
 if __name__ == "__main__":
-    link = input("Enter the link: ")
+    
+    #ask for the link from user
+    link = input("Enter the link of YouTube video you want to download:  ")
     yt = YouTube(link)
-    ys=yt.streams.get_highest_resolution()
-    ys.download('/Volumes/WD_BLACK/Lex')
+    
+    #Showing details
+    print("Title: ",yt.title)
+    print("Number of views: ",yt.views)
+    print("Length of video: ",yt.length)
+    print("Rating of video: ",yt.rating)
+    #Getting the highest resolution possible
+    ys = yt.streams.get_highest_resolution()
+    
+    #Starting download
+    print("Downloading...")
+    ys.download()
+    print("Download completed!!")
