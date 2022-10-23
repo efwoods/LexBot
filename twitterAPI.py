1843a5908e1f3d0c585be0aa2167ab51b5e03f3f
Evan Woods <22337004+efwoods@users.noreply.github.com>
2022-10-23 17:38:25 -0400

    Create twitterAPI.py

---
+++ twitterAPI.py
@@ -0,0 +1,17 @@
+
+# For sending GET requests from the API
+import requests
+# For saving access tokens and for file management when creating and adding to the dataset
+import os
+# For dealing with json responses we receive from the API
+import json
+# For displaying the data after
+import pandas as pd
+# For saving the response data in CSV format
+import csv
+# For parsing the dates received from twitter in readable formats
+import datetime
+import dateutil.parser
+import unicodedata
+#To add wait time between requests
+import time
