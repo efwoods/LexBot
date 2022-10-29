1c387a19acb3b8c130d05f4309a71cab7d2b1ac8
Evan Woods <22337004+efwoods@users.noreply.github.com>
2022-10-29 16:38:13 -0400

    commit

--- frontend/flask_app/app.py
+++ frontend/flask_app/app.py
@@ -1,6 +1,9 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, url_for, flash, redirect
+
+# ...
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'c3938b4bb28b00e59584c65e75218ad5ef4a0abf0dfdb238'
 
 messages = [{'title': 'Message One',
              'content': 'Message One Content'},
@@ -11,3 +14,23 @@ messages = [{'title': 'Message One',
 @app.route('/')
 def index():
     return render_template('index.html', messages=messages)
+
+# ...
+
+# ...
+
+@app.route('/create/', methods=('GET', 'POST'))
+def create():
+    if request.method == 'POST':
+        title = request.form['title']
+        content = request.form['content']
+
+        if not title:
+            flash('Title is required!')
+        elif not content:
+            flash('Content is required!')
+        else:
+            messages.append({'title': title, 'content': content})
+            return redirect(url_for('index'))
+
+    return render_template('create.html')
