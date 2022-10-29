ba78864163dfd033eff9a75a3fb5b6ef82793117
Evan Woods <22337004+efwoods@users.noreply.github.com>
2022-10-29 16:10:49 -0400

    commit

---
+++ frontend/flask_app/app.py
@@ -0,0 +1,13 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+messages = [{'title': 'Message One',
+             'content': 'Message One Content'},
+            {'title': 'Message Two',
+             'content': 'Message Two Content'}
+            ]
+
+@app.route('/')
+def index():
+    return render_template('index.html', messages=messages)
